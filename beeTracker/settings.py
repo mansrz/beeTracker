bd38aabd63805625e9dba1b73946e54c70d8876a
Juan Mite <juanmisak@gmail.com>
2015-01-17 21:40:49 -0500

    Fix production problems.

--- beeTracker/settings.py
+++ beeTracker/settings.py
@@ -59,8 +59,11 @@ WSGI_APPLICATION = 'beeTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'beeTrackerPostgres',
+        'USER': 'beetrackerapp',
+        'PASSWORD': 'BeeTrackerP0stgr3s',
+        'HOST': 'localhost',
     }
 }
 
